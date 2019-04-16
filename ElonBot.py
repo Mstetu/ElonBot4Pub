cbb244bb2ce74221d9e512775c0bf634cd1dc030
Mstetu <48696276+Mstetu@users.noreply.github.com>
2019-04-15 23:27:17 -0400

    Update ElonBot.py

--- ElonBot.py
+++ ElonBot.py
@@ -14,10 +14,10 @@ async def whymydolphinstopworkinglmao():
     await client.say('HAHAHAHAHAHHAHAHA')
 
 @client.command(pass_context=True)
-async def clear(ctx, amount-100):
-    channel - ctx.message.channel
-    messages - []
-    async for message in client.logs_from(channel, limit-int(amount)):
+async def clear(ctx, amount=100):
+    channel = ctx.message.channel
+    messages = []
+    async for message in client.logs_from(channel, limit=int(amount)):
         messages.append(message)
     await client.delete_messages(messages)
     await client.say('Messages deleted.')
